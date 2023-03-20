9de28e353ce91458819576a7ec8f774178940641
Ali-Eimaan <m.a.eimaan8@gmail.com>
2023-03-20 16:56:16 +0500

    Added LED Blink

--- 11_SYSTICK_DELAY.c
+++ 11_SYSTICK_DELAY.c
@@ -1,6 +1,20 @@
 #include "SYSTICK.h"
 
+#define GPIOCEN                 (1U<<4)
+#define PIN13                   (1U<<13)
+
 int main(void)
 {
-    SYSTICK_DelayMS(1000);
+    RCC->APB2ENR |= GPIOCEN;
+
+    GPIOC->CRH |= (1U<<20);
+    GPIOC->CRH |= (1U<<21);
+    GPIOC->CRH &=~ (1U<<22);
+    GPIOC->CRH &=~ (1U<<23);
+
+    while (1)
+    {
+        GPIOC->ODR ^= PIN13;
+        SYSTICK_DelayMS(1000);
+    }
 }
