a7aa75dfc8eb7ca5e8364dd40a890dbe30328d4c
Ali-Eimaan <116488986+Ali-Eimaan@users.noreply.github.com>
2023-03-24 21:46:19 +0500

    Update UART.c

--- UART_DRIVER/UART.c
+++ UART_DRIVER/UART.c
@@ -88,9 +88,9 @@ void DMA1_Channel4_INIT(uint32_t src, uint32_t des, uint32_t len)
     DMA1_Channel4->CMAR = src;
     DMA1_Channel4->CNDTR = len;
 
-    DMA1_Channel4->CR |= DMA1MINC;
-    DMA1_Channel4->CR |= DMA1TCIE;
-    DMA1_Channel4->CR |= DMA1CREN;
+    DMA1_Channel4->CCR |= DMA1MINC;
+    DMA1_Channel4->CCR |= DMA1TCIE;
+    DMA1_Channel4->CCR |= DMA1CREN;
 
     USART1->CR3 |= CR3_DMAT;
     NVIC_EnableIRQ(DMA1_Channel4_IRQn);
