08b6f164b1b80b2ad0107112a6438531fcd89e4c
Ali-Eimaan <116488986+Ali-Eimaan@users.noreply.github.com>
2023-03-26 16:21:54 +0500

    Create 21_I2C_MASTER.c

---
+++ 21_I2C_MASTER.c
@@ -0,0 +1,12 @@
+#include "I2C.h"
+
+uint8_t BUFF[8] = {0x48, 0x44, 0x88, 0x84, 0xE4, 0xE8, 0xEE, 0x8E};
+
+int main(void)
+{
+    I2C1_init();
+
+    I2C1_burstWrite(0xE8, 0x88, sizeof(BUFF), (char*) BUFF);
+
+    while (1){}
+}
